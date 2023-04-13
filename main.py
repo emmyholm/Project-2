d290e36190924059a9bf2cfddc7ee246f2b8b85d
Hampus <sodergren.hampus@gmail.com>
2023-04-13 11:25:51 +0200

    Added reading color of item

--- main.py
+++ main.py
@@ -39,38 +39,66 @@ ev3.speaker.beep()
 def ResetClawAngle():
    close_claw_speed = -100
 
-
-   # Set closed claw angle to 0 when at lowest position
+   # Set closed claw angle to 0 when closed
    claw_motor.run_until_stalled(close_claw_speed, then=Stop.HOLD, duty_limit=50)
    claw_motor.reset_angle(0)
 
+def ResetArmAngle():
+   raise_arm_speed = -100
 
+   # Set highest position as 0 degrees
+   arm_motor.run_until_stalled(raise_arm_speed, then=Stop.HOLD, duty_limit=50)
+   arm_motor.reset_angle(0)
+
+# US01 and US02
 def PickUpAndPutDown():
-
-
    close_claw_speed = -100
    open_claw_speed = 50
    raise_arm_speed = -100
    lower_arm_speed = 100
 
-
    ResetClawAngle()
 
-
-   # Open claw 90 degrees
+   # Open claw and insert item
    claw_motor.run_target(open_claw_speed, 90 ,then=Stop.HOLD)
    wait(5000)
 
-
-   # Pick up item (close claw)
+   # Pick up item
    claw_motor.run_until_stalled(close_claw_speed, then=Stop.HOLD, duty_limit=100)
    arm_motor.run_until_stalled(raise_arm_speed, then=Stop.HOLD, duty_limit=50)
-
 
    # Put down
    arm_motor.run_until_stalled(lower_arm_speed, then=Stop.HOLD, duty_limit=50)
    claw_motor.run_target(open_claw_speed, 90 ,then=Stop.HOLD)
    arm_motor.run_until_stalled(raise_arm_speed, then=Stop.HOLD, duty_limit=50)
+
+# US 04
+def PickUpAndReadColor():
+   close_claw_speed = -100
+   open_claw_speed = 50
+   raise_arm_speed = -100
+   lower_arm_speed = 100
+
+   ResetClawAngle()
+   ResetArmAngle()
+
+   # Lower arm and open claw to insert item
+   arm_motor.run_until_stalled(lower_arm_speed, then=Stop.HOLD, duty_limit=100)
+   claw_motor.run_target(50, 90 ,then=Stop.HOLD)
+   wait(5000)
+   
+   # Pick up item
+   claw_motor.run_until_stalled(close_claw_speed, then=Stop.HOLD, duty_limit=100)
+   arm_motor.run_until_stalled(raise_arm_speed, then=Stop.HOLD, duty_limit=50)
+
+   # Lower arm to color sensor and check color
+   arm_motor.run_target(50, 425, then=Stop.HOLD)
+   wait(5000)
+   print(color_sensor.color())
+
+
+
+   
 
 
 
@@ -86,4 +114,48 @@ def PickUpAndPutDown():
 # while True:
  # print(color_sensor.rgb()) # Kollar också vilken färg sensioren uppfattar men mer trovärdigt ger lite mer exakta värden
  # wait(500)
+
+
+
+
+# claw_motor.reset_angle(0)
+# wait(5000)
+
+
+#claw_motor.run_target(50, 45 ,then=Stop.HOLD)
+# wait(5000)
+
+
+
+
+
+# CHECK COLOR OF ITEM
+#claw_motor.run_until_stalled(-100, then=Stop.HOLD, duty_limit=50)
+#claw_motor.reset_angle(0)
+#arm_motor.run_until_stalled(100, then=Stop.HOLD, duty_limit=50)
+#arm_motor.reset_angle(0)
+#claw_motor.run_target(50, 90 ,then=Stop.HOLD)
+#wait(5000)
+#claw_motor.run_until_stalled(-50, then=Stop.HOLD, duty_limit=100)
+#arm_motor.run_until_stalled(-100, then=Stop.HOLD, duty_limit=50)
+#arm_motor.reset_angle(0)
+#print(arm_motor.angle())
+#arm_motor.run_target(50, 425, then=Stop.HOLD)
+#wait(5000)
+#print(color_sensor.color())
+#arm_motor.run_until_stalled(-100, then=Stop.HOLD, duty_limit=50)
+#wait(5000)
+#arm_motor.run_until_stalled(100, then=Stop.HOLD, duty_limit=50)
+#claw_motor.run_target(50, 90 ,then=Stop.HOLD)
+#arm_motor.run_until_stalled(-100, then=Stop.HOLD, duty_limit=50)
+
+
+
+
+
+
+#while True:5
+  # print(color_sensor()) # Hitta färger som finns runtomkring
+   #print(turning_motor.angle()) # Vilken vinkel som den befinner sig på
+   #wait(500)
 
