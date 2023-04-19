f5f5f2c7da7c405be8ae73cba546fb496d8826c4
Hampus <sodergren.hampus@gmail.com>
2023-04-19 11:17:49 +0200

    Added US09, 10

--- main.py
+++ main.py
@@ -18,17 +18,22 @@ turning_motor = Motor(Port.C)
 arm_motor = Motor(Port.B)
 claw_motor = Motor(Port.A)
 color_sensor = ColorSensor(Port.S2)
+touch_sensor = TouchSensor(Port.S1)
+
 close_claw_speed = -200
 open_claw_speed = 200
 raise_arm_speed = -200
 lower_arm_speed = 200
 clockwise_turning_speed = 200
 
-pick_up = -170
-first_drop_off = -460
-second_drop_off = -610
-third_drop_off = -720
-read_color_height = -480
+holding_object_claw_angle = 10
+
+pick_up = - 70
+read_color_height = -410
+
+first_drop_off = pick_up - 310
+second_drop_off = first_drop_off - 150
+third_drop_off = second_drop_off - 130
 
 ev3.speaker.beep()
 
@@ -55,7 +60,7 @@ def ResetRobot():
    claw_motor.run_target(open_claw_speed, 90, then=Stop.HOLD)
 
 def PickUpItem():
-   arm_motor.run_until_stalled(lower_arm_speed, then=Stop.HOLD, duty_limit=100)
+   arm_motor.run_until_stalled(lower_arm_speed, then=Stop.HOLD, duty_limit=20)
    claw_motor.run_until_stalled(close_claw_speed, then=Stop.HOLD, duty_limit=50)
    arm_motor.run_target(raise_arm_speed, read_color_height, then=Stop.HOLD)
 
@@ -63,6 +68,16 @@ def DropItem():
    arm_motor.run_until_stalled(lower_arm_speed, then=Stop.HOLD, duty_limit=50)
    claw_motor.run_target(open_claw_speed, 90, then=Stop.HOLD)
    arm_motor.run_target(raise_arm_speed, read_color_height, then=Stop.HOLD)
+
+def TestDropOff():
+   ResetRobot()
+
+   turning_motor.run_target(-200, first_drop_off, then=Stop.HOLD)
+   PickUpItem()
+   turning_motor.run_target(-200, second_drop_off, then=Stop.HOLD)
+   PickUpItem()
+   turning_motor.run_target(-200, third_drop_off, then=Stop.HOLD)
+   PickUpItem()
 
 def IsObjectInLocation(drop_off_zone):
    ResetRobot()
@@ -75,14 +90,14 @@ def IsObjectInLocation(drop_off_zone):
 
    claw_angle = claw_motor.angle()
 
-   if claw_angle > 0:
-      print("There is an object in the location")
+   if claw_angle > holding_object_claw_angle:
+      ev3.speaker.say("There is an object in the location")
       DropItem()
       
    else:
-      print("No object in location")
+      ev3.speaker.say("I can't find an object in the given location, sorry")
    
-def ReadColorAndDropInLocation():
+def ReadColor():
    ResetRobot()
    wait(1000)
 
@@ -91,29 +106,75 @@ def ReadColorAndDropInLocation():
 
    # read color
    arm_motor.run_target(raise_arm_speed, read_color_height, then=Stop.HOLD)
-   print(color_sensor.color())
    
+   current_color = str(color_sensor.color())
+   current_color = current_color.replace('Color.', '')
+
+   ev3.speaker.say('The color is ')
+   ev3.speaker.say(current_color)
+
    DropItem()
 
-def DropOffDependentOnColor():
+def DropOffDependentOnColor(seconds_delay_start, seconds_delay_between_attempts, total_attempts):
    ResetRobot()
+   claw_angle = 0
+   no_item_found_count = 0
 
-   PickUpItem()
+   if seconds_delay_start > 0:
+      ev3.speaker.say('Starting pick up process in ' + str(seconds_delay_start) + 'seconds')
+      wait(seconds_delay_start * 1000)
 
-   item_color = color_sensor.color()
-   print(item_color)
+   while no_item_found_count < total_attempts:
+      
+      # Try to pick up item
+      ev3.speaker.say('Trying to pick up item')
+      PickUpItem()
+      claw_angle = claw_motor.angle()
+
+      # No object found, wait and try again if attempts left
+      if claw_angle < holding_object_claw_angle:
+         no_item_found_count = no_item_found_count + 1
+
+         # Speaker message
+         speaker_message = 'No item found, ' + str(total_attempts - no_item_found_count) + 'attempts left '
+         if seconds_delay_between_attempts > 0 and no_item_found_count < total_attempts:
+            speaker_message = speaker_message + 'I will try again in ' + str(seconds_delay_between_attempts) + 'seconds'
+
+         ev3.speaker.say(speaker_message)
+
+         # Delay time
+         wait(seconds_delay_between_attempts * 1000)
+
+         # Open claw again
+         claw_motor.run_target(open_claw_speed, 90, then=Stop.HOLD)
+
+      # Item found
+      else:
+         item_color = color_sensor.color()
+         ev3.speaker.say(str(color_sensor.color()).replace('Color.', ''))
+         
+         # Turn dependent on color
+         if item_color == Color.RED:
+            turning_motor.run_target(-200, first_drop_off, then=Stop.HOLD)
+
+         elif item_color == Color.GREEN:
+            turning_motor.run_target(-200, second_drop_off, then=Stop.HOLD)
+
+         elif item_color == Color.YELLOW:
+            turning_motor.run_target(-200, third_drop_off, then=Stop.HOLD)
+
+         elif item_color == Color.BLUE:
+            turning_motor.run_target(-200, third_drop_off, then=Stop.HOLD)
+
+         else:
+            ev3.speaker.say('Unknown Color')
+         
+         DropItem()
+
+         # Return to pick up location
+         turning_motor.run_target(-200, pick_up, then=Stop.HOLD)
    
-   if item_color == Color.RED:
-      turning_motor.run_target(-200, first_drop_off, then=Stop.HOLD)
-   elif item_color == Color.GREEN:
-      turning_motor.run_target(-200, second_drop_off, then=Stop.HOLD)
-   elif item_color == Color.YELLOW:
-      turning_motor.run_target(-200, third_drop_off, then=Stop.HOLD)
-   elif item_color == Color.BLUE:
-      turning_motor.run_target(-200, third_drop_off, then=Stop.HOLD)
-   
-   DropItem()
-
+DropOffDependentOnColor(5, 2, 2)
 
 
 
@@ -151,8 +212,3 @@ def PickUpAndPutDown():
    arm_motor.run_until_stalled(lower_arm_speed, then=Stop.HOLD, duty_limit=50)
    claw_motor.run_target(open_claw_speed, 90 ,then=Stop.HOLD)
    arm_motor.run_until_stalled(raise_arm_speed, then=Stop.HOLD, duty_limit=50)
-
-
-
-
-#IsObjectInLocation(second_drop_off)
